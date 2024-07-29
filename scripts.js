0ee9b5c2ad65c8267b336c5769a62f3d52706d4f
Soulaymane01 <soulaynadi@gmail.com>
2024-07-29 21:01:02 +0100

    initial commit

---
+++ scripts.js
@@ -0,0 +1,55 @@
+// Swiper initialization
+const swiper = new Swiper('.swiper-container', {
+    loop: true, // Enable looping
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 1,
+
+    spaceBetween: 10,
+    centeredSlides: true,
+});
+
+
+// Ensuring images have consistent width
+document.addEventListener('DOMContentLoaded', function() {
+    const swiperSlides = document.querySelectorAll('.swiper-slide img');
+    let maxWidth = 0;
+
+    swiperSlides.forEach(img => {
+        if (img.naturalWidth > maxWidth) {
+            maxWidth = img.naturalWidth;
+        }
+    });
+
+    swiperSlides.forEach(img => {
+        img.style.width = maxWidth + 'px';
+    });
+});
+
+
+
+
+
+window.addEventListener('load', function() {
+    // Get all images in the Swiper container
+    const images = document.querySelectorAll('.swiper-slide img');
+    let maxWidth = 0;
+
+    // Loop through images to find the maximum width
+    images.forEach(image => {
+        if (image.width > maxWidth) {
+            maxWidth = image.width;
+        }
+    });
+
+    // Set all images to the maximum width
+    images.forEach(image => {
+        image.style.width = maxWidth + 'px';
+    });
+});
